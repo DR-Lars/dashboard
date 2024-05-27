7506e888afffd19167a6e87a1abe78bb647134d3
Lars De Roover <67840433+DR-Lars@users.noreply.github.com>
2024-05-28 00:01:04 +0200

    Add temperature functionality

--- src/app/measurement.service.ts
+++ src/app/measurement.service.ts
@@ -1,17 +1,22 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MeasurementService {
-  private apiUrl =
-    'http://api.derooverit.com/latest-measurement/station=ANT001BE';
+  private apiUrl = '/api/latest-measurement/station=ANT001BE';
 
   constructor(private http: HttpClient) {}
 
   getLatestMeasurement(): Observable<any> {
-    return this.http.get<any>(this.apiUrl);
+    return this.http.get<any>(this.apiUrl).pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    console.error('An error occurred:', error.error.message);
+    return throwError('Something went wrong; please try again later.');
   }
 }
--- src/app/temperature/temperature.component.ts
+++ src/app/temperature/temperature.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MeasurementService } from '../measurement.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-temperature',
   templateUrl: './temperature.component.html',
-  styleUrls: ['./temperature.component.css']
+  styleUrls: ['./temperature.component.css'],
 })
-export class TemperatureComponent {
+export class TemperatureComponent implements OnInit {
+  latestMeasurement!: Observable<any>;
 
+  constructor(private MeasurementService: MeasurementService) {}
+
+  ngOnInit(): void {
+    this.latestMeasurement = this.MeasurementService.getLatestMeasurement();
+  }
 }
