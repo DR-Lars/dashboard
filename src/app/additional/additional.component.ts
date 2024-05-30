eefc2469ae4654fdbf9381f4aabcfaf00823736c
Lars De Roover <67840433+DR-Lars@users.noreply.github.com>
2024-05-30 09:59:53 +0200

    Fix general UI

--- src/app/additional/additional.component.ts
+++ src/app/additional/additional.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { MeasurementService } from '../measurement.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-additional',
   templateUrl: './additional.component.html',
-  styleUrls: ['./additional.component.css']
+  styleUrls: ['./additional.component.css'],
 })
 export class AdditionalComponent {
+  latestMeasurement!: Observable<any>;
+  screenHeight!: number;
+  screenWidth!: number;
 
+  constructor(private MeasurementService: MeasurementService) {}
+
+  ngOnInit(): void {
+    this.latestMeasurement = this.MeasurementService.getLatestMeasurement();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.screenHeight = window.innerHeight;
+    this.screenWidth = window.innerWidth;
+  }
 }
